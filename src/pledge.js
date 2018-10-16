a4ec1917778b85e3cac9b24ddeb4fa4e65e2f01b
Veronica Smith <veronica.smith@ruralsource.com>
2018-10-16 09:41:48 -0400

    chapter 1

--- src/pledge.js
+++ src/pledge.js
@@ -3,6 +3,59 @@
 Promises Workshop: build the pledge.js ES6-style promise library
 ----------------------------------------------------------------*/
 // YOUR CODE HERE:
+function $Promise(exec) {
+    if (typeof exec !== 'function') throw new TypeError('/executor.+function/i');
+
+    this._exec = new Promise(exec);
+    this._state = 'pending';
+    exec(
+        this._internalResolve.bind(this),
+        this._internalReject.bind(this)
+    )
+}
+
+$Promise.prototype._settle = function (data, state) {
+    if (this._state === 'pending') {
+        this._state = state;
+        this._value = data;
+    }
+}
+
+$Promise.prototype._internalResolve = function (data) {
+    this._settle(data, 'fulfilled')
+}
+$Promise.prototype._internalReject = function (err) {
+    this._settle(err, 'rejected');
+}
+
+
+
+
+
+// function $Promise(exec) {
+//     if (typeof exec !== 'function') throw new TypeError('/executor.+function/i');
+//     this._exec = new Promise(exec);
+//     this._state = 'pending';
+//     exec(
+//         this._internalResolve.bind(this),
+//         this._internalReject.bind(this)
+//     );
+
+// }
+
+// $Promise.prototype._settle = function (value, state) {
+//     if (this._state === 'pending') {
+//         this._state = state;
+//         this._value = value;
+//     }
+// }
+
+// $Promise.prototype._internalResolve = function (data) {
+//     this._settle(data, 'fulfilled');
+// };
+// $Promise.prototype._internalReject = function (err) {
+//     this._settle(err, 'rejected');
+// };
 
 
 
